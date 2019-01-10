c4d4d31722228106635d4eaa9f058c7a2bb62036
cmmadnat <cmmadnat@gmail.com>
2019-01-10 23:45:30 +0700

    add redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
 
 import './index.css'
 import MockDataProvider from './MockDataProvider'
 import TerminateModalFlow from './TerminateModalFlow.react'
+import transferReducer from './containers/TransferScreen/reducers'
+
+const store = createStore(combineReducers({ transferReducer }))
 
 ReactDOM.render(
-  <MockDataProvider>
-    {props => <TerminateModalFlow {...props} />}
-  </MockDataProvider>,
+  <Provider store={store}>
+    <MockDataProvider>
+      {props => <TerminateModalFlow {...props} />}
+    </MockDataProvider>
+  </Provider>,
   document.getElementById('root')
 )
 
